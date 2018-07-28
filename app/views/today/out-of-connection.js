045fa5e4ee289810d2c7ab8950ea86bccaf32b31
kf <7kfpun@gmail.com>
2018-07-28 15:28:06 +0800

    Add locales

--- app/views/today/out-of-connection.js
+++ app/views/today/out-of-connection.js
@@ -9,6 +9,8 @@ import {
 import { iOSColors } from 'react-native-typography';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
+import I18n from '../../utils/i18n';
+
 const styles = StyleSheet.create({
   text: {
     textAlign: 'center',
@@ -23,7 +25,7 @@ const styles = StyleSheet.create({
 const OutOfConnection = () => (
   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
     <Ionicons name="ios-construct-outline" size={30} color={iOSColors.black} />
-    <Text style={styles.text}>Out of connection</Text>
+    <Text style={styles.text}>{I18n.t('app.today.out_of_connection')}</Text>
   </View>
 );
 
