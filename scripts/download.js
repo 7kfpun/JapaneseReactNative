d8733c8a34c7bfe6a65a10c18adca940911357bd
kf <7kfpun@gmail.com>
2018-10-08 21:15:16 +0800

    Add scripts for uploading and downloading locales

---
+++ scripts/download.js
@@ -0,0 +1,36 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+const md5 = require('md5');
+const queryString = require('querystring');
+
+const { config } = require('../app/config');
+
+const PROJECT_ID = 316516;
+const FILE_NAME = 'app.json';
+const OUTPUT_LOCATION = './app/utils/locales/';
+const LOCALES = ['en', 'zh', 'zh-Hant', 'de', 'vi', 'my'];
+
+const PUBLIC_KEY = config.oneskyApiKey;
+const SECRET_KEY = config.oneskySecretKey;
+
+async function downloadFile(locale) {
+  const timestamp = Date.now() / 1000;
+  const devHash = md5(timestamp + SECRET_KEY);
+  const url = `https://platform.api.onesky.io/1/projects/${PROJECT_ID}/translations?${queryString.stringify(
+    {
+      timestamp,
+      locale,
+      api_key: PUBLIC_KEY,
+      dev_hash: devHash,
+      source_file_name: FILE_NAME,
+    }
+  )}`;
+
+  const body = await fetch(url);
+  const text = await body.text();
+  fs.writeFile(`${OUTPUT_LOCATION}/${locale}/app.json`, text, 'utf8', () =>
+    console.log('Download:', locale)
+  );
+}
+
+LOCALES.forEach(lang => downloadFile(lang));
---
+++ scripts/upload.js
@@ -0,0 +1,36 @@
+const FormData = require('form-data');
+const fetch = require('node-fetch');
+const fs = require('fs');
+const md5 = require('md5');
+const queryString = require('querystring');
+
+const PROJECT_ID = 316516;
+const FILE_NAME = './app/utils/locales/en/app.json';
+
+const PUBLIC_KEY = config.oneskyApiKey;
+const SECRET_KEY = config.oneskySecretKey;
+
+function updateFile() {
+  const timestamp = Date.now() / 1000;
+  const devHash = md5(timestamp + SECRET_KEY);
+
+  const url = `https://platform.api.onesky.io/1/projects/${PROJECT_ID}/files?${queryString.stringify(
+    {
+      timestamp,
+      api_key: PUBLIC_KEY,
+      dev_hash: devHash,
+    }
+  )}`;
+
+  const form = new FormData();
+  form.append('file', fs.createReadStream(FILE_NAME));
+  form.append('file_format', 'HIERARCHICAL_JSON');
+  form.append('locale', 'en');
+
+  console.log(url);
+  fetch(url, { method: 'POST', body: form })
+    .then(res => res.json())
+    .then(json => console.log(json));
+}
+
+updateFile();
