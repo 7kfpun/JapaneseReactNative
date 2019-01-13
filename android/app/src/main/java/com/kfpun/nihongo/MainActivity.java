3c6048e87496dd3de6455f1adcd6e477cac609d6
kf <7kfpun@gmail.com>
2019-01-13 17:47:40 +0800

    Add branch, fbsdk and google-signin

--- android/app/src/main/java/com/kfpun/nihongo/MainActivity.java
+++ android/app/src/main/java/com/kfpun/nihongo/MainActivity.java
@@ -1,5 +1,10 @@
 package com.kfpun.nihongo;
 
+import io.branch.rnbranch.*;
+import android.content.Intent;
+
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -11,5 +16,25 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "Japanese";
+    }
+
+    // Override onStart, onNewIntent:
+    @Override
+    protected void onStart() {
+        super.onStart();
+        RNBranchModule.initSession(getIntent().getData(), this);
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+    }
+
+    // Facebook SDK
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
     }
 }
--- android/app/src/main/java/com/kfpun/nihongo/MainApplication.java
+++ android/app/src/main/java/com/kfpun/nihongo/MainApplication.java
@@ -2,6 +2,7 @@ package com.kfpun.nihongo;
 
 import android.app.Application;
 
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.avishayil.rnrestart.ReactNativeRestartPackage;
 import com.clevertap.android.sdk.ActivityLifecycleCallback;
@@ -9,14 +10,20 @@ import com.clevertap.react.CleverTapPackage;
 import com.crashlytics.android.answers.Answers;
 import com.crashlytics.android.Crashlytics;
 import com.dooboolab.RNIap.RNIapPackage;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.CallbackManager;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.smixx.fabric.FabricPackage;
+import io.branch.referral.Branch;
+import io.branch.rnbranch.RNBranchPackage;
 import io.fabric.sdk.android.Fabric;
 import io.invertase.firebase.admob.RNFirebaseAdMobPackage;
-import io.invertase.firebase.config.RNFirebaseRemoteConfigPackage;
 import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import io.invertase.firebase.config.RNFirebaseRemoteConfigPackage;
 import io.invertase.firebase.fabric.crashlytics.RNFirebaseCrashlyticsPackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import io.invertase.firebase.perf.RNFirebasePerformancePackage;
@@ -34,6 +41,12 @@ import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
+  // Facebook SDK
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
+
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -46,16 +59,20 @@ public class MainApplication extends Application implements ReactApplication {
             new MainReactPackage(),
             new CleverTapPackage(),
             new FabricPackage(),
+            new FBSDKPackage(mCallbackManager),
             new ReactNativeOneSignalPackage(),
             new ReactNativeRestartPackage(),
+            new RNBranchPackage(),
             new RNDeviceInfo(),
             new RNFirebaseAdMobPackage(),
             new RNFirebaseAnalyticsPackage(),
+            new RNFirebaseAuthPackage(),
             new RNFirebaseCrashlyticsPackage(),
             new RNFirebaseMessagingPackage(),
             new RNFirebasePackage(),
             new RNFirebasePerformancePackage(),
             new RNFirebaseRemoteConfigPackage(),
+            new RNGoogleSigninPackage(),
             new RNI18nPackage(),
             new RNIapPackage(),
             new TextToSpeechPackage(),
@@ -79,6 +96,13 @@ public class MainApplication extends Application implements ReactApplication {
     Fabric.with(this, new Crashlytics(), new Answers());
     SoLoader.init(this, /* native exopackage */ false);
     ActivityLifecycleCallback.register(this);
+
+    // Initialize the Branch object
+    Branch.getAutoInstance(this);
+
     super.onCreate();
+
+    // Facebook SDK
+    AppEventsLogger.activateApp(this);
   }
 }
